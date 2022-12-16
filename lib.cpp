5ab1c3906ede96f931c37732c58c35babd402004
benve03 <117894307+benve03@users.noreply.github.com>
2022-12-16 21:47:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
+#include "cstring"
+#include "lib.h"
 
+
+using namespace std;
+int funzione (char nomi[10][20], char ricerca[20]) {
+
+    for (int i = 0; i < 10; i++) {
+        if( strcmp (nomi[i], ricerca)==0){
+            return i;
+        }
+    }
+    return 11;
+}
