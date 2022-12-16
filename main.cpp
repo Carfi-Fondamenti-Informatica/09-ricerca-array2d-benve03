e2da909520f0f979a4ec83066168245405687fc6
benve03 <117894307+benve03@users.noreply.github.com>
2022-12-16 21:49:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
+#include "lib.h"
 #include <iostream>
-
-int main(){
-  return 0;
+using namespace std;
+int main() {
+    char nomi[10][20];
+    char ricerca[20];
+    for(int i =0; i<10; i++){
+        cin >> nomi[i];
+        }
+    cin >> ricerca;
+    int s = funzione (nomi, ricerca);
+    if (s<=10){
+        cout<<s;
+    }
+    else{
+        cout<<"non presente";
+    }
 }
